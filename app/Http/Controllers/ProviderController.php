46286925ef78dd48923546fafa809336f71bbce2
AgustinZavalaA <56318577+AgustinZavalaA@users.noreply.github.com>
2021-12-18 23:22:05 -0600

    CRUD modelo Provider (tiene que cambiar)

---
+++ app/Http/Controllers/ProviderController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Provider;
+
+class ProviderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $providers = Provider::all();
+        return $providers;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $provider = new Provider();
+        $provider->name = $request->name;
+        $provider->email = $request->email;
+
+        $provider->save();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request)
+    {
+        $provider = Provider::findOrFail($id);
+        $provider->name = $request->name;
+        $provider->email = $request->email;
+
+        $provider->save();
+        return $provider; 
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request)
+    {
+        $provider = Provider::destroy($request->id);
+        return $provider;
+    }
+}
